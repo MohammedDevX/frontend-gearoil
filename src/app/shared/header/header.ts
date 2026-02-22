4c91b49aecc3a64f43c138d9aee483e1ff4d8513
AyoubAZ <ayoubazamri0@gmail.com>
2026-02-22 14:42:07 +0100

    NavBar Trying to fix NgIf Deprecared

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { NgIf, NgIfContext } from "@angular/common";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+    isDepartmentsOpen = false;
+
+    toggleDepartments(event: Event) {
+      event.stopPropagation();
+      this.isDepartmentsOpen = !this.isDepartmentsOpen;
+    }
+
+    closeDepartments() {
+      this.isDepartmentsOpen = false;
+    }
+
 
 }
