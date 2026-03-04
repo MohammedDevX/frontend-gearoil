7f5eb9a99b67dea99043f97e468b250bc135e506
AyoubAZ <ayoubazamri0@gmail.com>
2026-03-05 00:27:46 +0100

    first sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { AppSidebarComponent } from './shared/app-sidebar/app-sidebar.component';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  {path: 'sidebar', component: AppSidebarComponent}
 ];
